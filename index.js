712237d3afeb1d2aedf49afe1e237c76f18a48cd
PoseiDon <suriya.b@kkumail.com>
2024-12-30 01:58:39 +0700

    Refactor session management to use connect-mongo for session storage and improve MongoDB connection options

--- index.js
+++ index.js
@@ -3,38 +3,47 @@ const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const expressSession = require('express-session'); 
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-const PORT = 4000;
 const serverless = require('serverless-http');
 
-
-//Database connection
+// Database connection
 mongoose.connect('mongodb+srv://admin:adminWEB10@cluster0.3obax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    ttl: 24 * 60 * 60 // กำหนดเวลาหมดอายุของ session (1 วัน)
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
-    console.error('Failed to connect to MongoDB', err);
+    console.error('Failed to connect to MongoDB:', err);
 });
 
 global.loggedIn = null;
 
+// Middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
-app.use(expressSession({ 
-    secret: 'secret', 
-    resave: false, 
-    saveUninitialized: false 
-}));
-app.use("*", (req, res, next) => {
+app.use(
+    expressSession({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: 'mongodb+srv://admin:adminWEB10@cluster0.3obax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        }),
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000 // 1 วัน (ในมิลลิวินาที)
+        }
+    })
+);
+app.use('*', (req, res, next) => {
     loggedIn = req.session.userId;
     next();
 });
 
-//Set views
+// Set views
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('controllers', path.resolve(__dirname, 'controllers'));
@@ -42,8 +51,7 @@ app.set('middleware', path.resolve(__dirname, 'middleware'));
 app.set('public', path.resolve(__dirname, 'public'));
 app.set('models', path.resolve(__dirname, 'models'));
 
-
-//Controllers
+// Controllers
 const indexController = require('./controllers/indexController');
 const loginController = require('./controllers/loginController');
 const registerController = require('./controllers/registerController');
@@ -52,11 +60,11 @@ const loginUserController = require('./controllers/loginUserController');
 const logoutController = require('./controllers/logoutController');
 const homeController = require('./controllers/homeController');
 
-//Middleware
+// Middleware
 const redirectIfAuth = require('./middleware/redirectifAuth');
 const authMiddleware = require('./middleware/authMiddleware');
 
-//Routes
+// Routes
 app.get('/', indexController);
 app.get('/login', redirectIfAuth, loginController);
 app.get('/register', redirectIfAuth, registerController);
@@ -65,20 +73,10 @@ app.post('/user/login', redirectIfAuth, loginUserController);
 app.get('/logout', logoutController);
 app.get('/home', authMiddleware, homeController);
 
-
-// app.listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}`);
-// });
-
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
-
-// module.exports = (req, res) => {
-//     app(req, res);
-// };
-
 
 module.exports = serverless(app);
